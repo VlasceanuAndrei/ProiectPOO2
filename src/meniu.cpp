b11f5e5e313e67ca713ef920581eca69ad14927c
VlasceanuAndrei <57728913+VlasceanuAndrei@users.noreply.github.com>
2025-01-12 18:01:09 +0200

    Update meniu.cpp
    includere metode permsiuni + venit
    adaugarea unui vector de useri pentru a putea demonstra rezultatul diferit al functiilor virtuale

--- src/meniu.cpp
+++ src/meniu.cpp
@@ -7,19 +7,20 @@ Meniu& Meniu::inst() {
     return *m;
 }
 void Meniu::run() {
+    std::vector<User> u;
     std::vector<Profesor> p;
     std::vector<Elev> e;
     std::cout << "0. Stop\n"
                  "1. Adauga profesor\n"
                  "2. Adauga elev\n"
-                 "3. Adauga nota\n"
-                 "4. Vizualizeaza note\n"
+                 "3. Adauga user\n"
+                 "4. Permisiuni particulare user\n"
                  "5. Vizualizeaza media generala\n"
                  "6. Vizualizeaza notele pentru o zi\n"
                  "7. Sorteaza si afiseaza elevii in ordine lexicografica inversa\n"
-                 "8. Modifica nota\n"
-                 "9. Sorteaza descrescator profesorii dupa lungimea numelui si a prenumelui\n"
-                 "10. Afiseaza data nasterii pentru un utilizator";
+                 "8. Sorteaza descrescator profesorii dupa lungimea numelui si a prenumelui\n"
+                 "9. Afiseaza data nasterii pentru un utilizator\n"
+                 "10. Afisare venit pentru user";
     int caz = 1;
     while (caz) {
         std::cin >> caz;
@@ -37,23 +38,59 @@ void Meniu::run() {
                 break;
             }
             case 3: {
-                std::cout << "Introduceti indexul profesorului (maxim: " << static_cast<int>(p.size()) - 1 << "): ";
-                int indexP;
-                std::cin >> indexP;
-                std::cout << "Introduceti indexul elevului (maxim: " << static_cast<int>(e.size()) - 1 << "): ";
-                int indexE;
-                std::cin >> indexE;
-                if (indexE < static_cast<int>(e.size()) && indexP < static_cast<int>(p.size()))
-                    p[indexP].adaugaNotaElev(e[indexE]);
-                else std::cout << "Valorile introduse pentru index nu sunt valide.\n";
+                User u1;
+                std::cin >> u1;
+                u.push_back(u1);
                 break;
             }
             case 4: {
-                std::cout << "Introduceti indexul elevului (maxim: " << static_cast<int>(e.size()) - 1 << "): ";
-                int indexE;
-                std::cin >> indexE;
-                if (indexE < static_cast<int>(e.size()))
-                    e[indexE].afiseazaNote();
+                std::cout << "Vizualizare permisiuni pentru user/profesor/elev? (u/p/e): ";
+                char tip;
+                std::cin >> tip;
+                if (tolower(tip) == 'u') {
+                    std::cout << "Introduceti indexul userului (maxim: " << static_cast<int>(u.size()) - 1 << "): ";
+                    int indexU;
+                    std::cin >> indexU;
+                    if (indexU < static_cast<int>(u.size()))
+                        u[indexU].arePermisiuni();
+                    else std::cout << "Indexul nu este valid.\n";
+                }
+                else if (tolower(tip) == 'p') {
+                    std::cout << "Introduceti indexul profesorului (maxim: " << static_cast<int>(p.size()) - 1 << "): ";
+                    int indexP;
+                    std::cin >> indexP;
+                    if (indexP < static_cast<int>(p.size())) {
+                        p[indexP].arePermisiuni();
+                        int caz;
+                        std::cin >> caz;
+                        int indexE;
+                        if (caz == 0 || caz == 1) {
+                            std::cout << "Introduceti indexul elevului (maxim: " << static_cast<int>(e.size()) - 1 << "): ";
+                            std::cin >> indexE;
+                            if (indexE >= static_cast<int>(e.size()))
+                                std::cout << "Indexul nu este valid.\n";
+                        }
+                        if (caz == 0)
+                            p[indexP].adaugaNotaElev(e[indexE]);
+                        else if (caz == 1) {
+                            int zi, luna, an;
+                            std::cout << "Introduceti data: ";
+                            std::cin >> zi >> luna >> an;
+                            double val;
+                            std::cout << "Nota cautata: ";
+                            std::cin >> val;
+                            p[indexP].modificaNotaElev(e[indexE], zi, luna, an, val);
+                        }
+                    }
+                }
+                else if (tolower(tip) == 'e') {
+                    std::cout << "Introduceti indexul elevului (maxim: " << static_cast<int>(e.size()) - 1 << "): ";
+                    int indexE;
+                    std::cin >> indexE;
+                    if (indexE < static_cast<int>(e.size()))
+                        e[indexE].arePermisiuni();
+                    else std::cout << "Indexul nu este valid.\n";
+                    }
                 else std::cout << "Indexul nu este valid.\n";
                 break;
             }
@@ -88,24 +125,6 @@ void Meniu::run() {
                 break;
             }
             case 8: {
-                std::cout << "Introduceti indexul profesorului (maxim: " << static_cast<int>(p.size()) - 1 << "): ";
-                int indexP;
-                std::cin >> indexP;
-                std::cout << "Introduceti indexul elevului (maxim: " << static_cast<int>(e.size()) - 1 << "): ";
-                int indexE;
-                std::cin >> indexE;
-                std::cout << "Introduceti data pentru modificarea notei: ";
-                int zi, luna, an;
-                std::cin >> zi >> luna >> an;
-                std::cout << "Introduceti nota cautata: ";
-                int nota;
-                std::cin >> nota;
-                if (indexE < static_cast<int>(e.size()) && indexP < static_cast<int>(p.size()))
-                    p[indexP].modificaNotaElev(e[indexE], zi, luna, an, nota);
-                else std::cout << "Valorile introduse pentru index nu sunt valide.\n";
-                break;
-            }
-            case 9: {
                 std::sort(p.begin(), p.end(), [](const Profesor& p1, const Profesor& p2) {
                     return p1 > p2;
                 });
@@ -113,16 +132,54 @@ void Meniu::run() {
                     std::cout << p[i];
                 break;
             }
-            case 10: {
-                std::cout << "Vizualizare data pentru profesor sau elev? (p/e): ";
-                char val;
-                std::cin >> val;
-                if (tolower(val) == 'p') {
+            case 9: {
+                std::cout << "Vizualizare data nasterii pentru user/profesor/elev? (u/p/e): ";
+                char tip;
+                std::cin >> tip;
+                if (tolower(tip) == 'u') {
+                    std::cout << "Introduceti indexul userului (maxim: " << static_cast<int>(u.size()) - 1 << "): ";
+                    int indexU;
+                    std::cin >> indexU;
+                    if (indexU < static_cast<int>(u.size()))
+                        u[indexU].afiseazaDataNasterii();
+                    else std::cout << "Indexul nu este valid.\n";
+                }
+                else if (tolower(tip) == 'p') {
                     std::cout << "Introduceti indexul profesorului (maxim: " << static_cast<int>(p.size()) - 1 << "): ";
                     int indexP;
                     std::cin >> indexP;
                     if (indexP < static_cast<int>(p.size()))
                         p[indexP].afiseazaDataNasterii();
+                }
+                else if (tolower(tip) == 'e') {
+                    std::cout << "Introduceti indexul elevului (maxim: " << static_cast<int>(e.size()) - 1 << "): ";
+                    int indexE;
+                    std::cin >> indexE;
+                    if (indexE < static_cast<int>(e.size()))
+                        e[indexE].afiseazaDataNasterii();
+                    else std::cout << "Indexul nu este valid.\n";
+                }
+                else std::cout << "Indexul nu este valid.\n";
+                break;
+            }
+            case 10: {
+                std::cout << "Vizualizare venit pentru user/profesor/elev (u/p/e): ";
+                char val;
+                std::cin >> val;
+                if (tolower(val) == 'u') {
+                    std::cout << "Introduceti indexul userului (maxim: " << static_cast<int>(u.size()) - 1 << "): ";
+                    int indexU;
+                    std::cin >> indexU;
+                    if (indexU < static_cast<int>(u.size()))
+                        u[indexU].areVenit();
+                    else std::cout << "Indexul nu este valid.\n";
+                }
+                else if (tolower(val) == 'p') {
+                    std::cout << "Introduceti indexul profesorului (maxim: " << static_cast<int>(p.size()) - 1 << "): ";
+                    int indexP;
+                    std::cin >> indexP;
+                    if (indexP < static_cast<int>(p.size()))
+                        p[indexP].areVenit();
                     else std::cout << "Indexul nu este valid.\n";
                 }
                 else if (tolower(val) == 'e') {
@@ -130,7 +187,7 @@ void Meniu::run() {
                     int indexE;
                     std::cin >> indexE;
                     if (indexE < static_cast<int>(e.size()))
-                        e[indexE].afiseazaDataNasterii();
+                        e[indexE].areVenit();
                     else std::cout << "Indexul nu este valid.\n";
                 }
                 break;
